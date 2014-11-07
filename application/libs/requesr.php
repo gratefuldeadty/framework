a06f8880e3f33f19d369e518df3e2852ae3e94de
gratefuldeadty <gratefuldeadty@gmail.com>
2014-11-06 22:25:05 -0500

    Update requesr.php

--- application/libs/requesr.php
+++ application/libs/requesr.php
@@ -1,51 +1,61 @@
+<?php
+
 class Request
 {
-/**
-* Tests if the current request is a POST request
-* @return boolean
-*/
-public function isPost()
-{
-return ($_SERVER['REQUEST_METHOD'] == 'POST' ? true : false);
-}
-/**
-* Tests if the current request is a GET request
-* @return boolean
-*/
-protected function _isGet()
-{
-return ($_SERVER['REQUEST_METHOD'] == 'GET' ? true : false);
-}
-/**
-* fetches the given parameter data.
-* @param string $key the key to look for.
-* @param mixed $default the default value to return, if the given parameter is not set.
-*/
-public function getParam($key, $default = null)
-{
-if ($this->isPost()) {
-if(isset($_POST[$key])) {
-return $_POST[$key];
-}
-}
-else if ($this->_isGet()) {
-if(isset($_GET[$key])) {
-return $_GET[$key];
-}
-}
-return $default;
-}
-/**
-* Returns a list of parameters given in the current request
-* @return array the params given
-*/
-public function getAllParams()
-{
-if ($this->isPost()) {
-return $_POST;
-}
-else if ($this->_isGet()) {
-return $_GET;
-}
-}
+        /**
+         * Check if the server request method is indeed post.
+         */
+        public function isPost()
+        {
+                return (($_SERVER['REQUEST_METHOD']) == 'POST') ? true : false;
+        }
+        
+        /**
+         * Check if the server request method is indeed get
+         */
+        public function isGet()
+        {
+                return (($_SERVER['REQUEST_METHOD']) == 'GET') ? true : false;
+        }
+        
+        /**
+         * Post
+         * @param string $key
+         * @return $_POST : false
+         */
+        public function post($key)
+        {
+                if ($this->isPost())
+                {
+                        return (isset($_POST[$key])) ? $_POST[$key] : false;
+                }
+        }
+        
+        /**
+         * Get
+         * @param string $key
+         * @return $_GET : false
+         */
+        public function get($key)
+        {
+                if ($this->isGet())
+                {
+                        return (isset($_GET[$key])) ? $_GET[$key] : false;
+                }
+        }
+        
+        /**
+         * Get all $_POST and $_GET params
+         */
+        public function allParams()
+        {
+                if ($this->isPost())
+                {
+                        return $_POST;
+                }
+                elseif ($this->isGet())
+                {
+                        return $_GET;
+                }
+        }
 }
