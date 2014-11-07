5187a25dc4a89bafa863b9fc08fa4abc1a0d3978
gratefuldeadty <gratefuldeadty@gmail.com>
2014-11-06 22:04:30 -0500

    Update view.php

--- application/libs/view.php
+++ application/libs/view.php
@@ -1,1 +1,26 @@
 <?php
+
+class View
+{
+        /**
+         * Render the page being viewed.
+         * @param string $filename
+         * @param bool $blank_render
+         * @param array $data
+         * @return require
+         */
+        public function render($filename, $blank_render = false, $data[])
+        {
+                if ($blank_render == true)
+                {
+                        require 'application/views/' . $filename . '.php';
+                }
+                else
+                {
+                        require 'application/views/templates/header.php';
+                        require 'application/views/' . $filename . '.php';
+                        require 'application/views/templates/footer.php';
+                }
+        }
+}
+
