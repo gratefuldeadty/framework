710ebd4c569359867bf3aa8401204d08f6f182ae
gratefuldeadty <gratefuldeadty@gmail.com>
2014-11-09 02:11:26 -0500

    Update requestdata.php

--- game/network/requestdata.php
+++ game/network/requestdata.php
@@ -1,55 +1,12 @@
-namespace OpCacheGUI\Network;
+<?php
 
-/**
- * Interface for request classes
- *
- * @category   OpCacheGUI
- * @package    Network
- * @author     Pieter Hordijk <info@pieterhordijk.com>
- */
+namespace game\network;
 interface RequestData
 {
-    /**
-     * Gets the variable or returns false when it does not exists
-     *
-     * @return boolean|string The value of the get param or false when it does not exist
-     */
-    public function get();
-
-    /**
-     * Gets the variable
-     *
-     * @return string The value of the path param
-     */
-    public function path();
-
-    /**
-     * Gets the verb of the request
-     *
-     * @return string The verb used by the request
-     */
-    public function getVerb();
-
-    /**
-     * Gets a post variable
-     *
-     * @param string $name The name of the post variable to get
-     *
-     * @return mixed The value
-     */
-    public function post($name);
-
-    /**
-     * Gets the current URL
-     *
-     * @return string The current URL
-     */
-    public function getUrl();
-
-    /**
-     * Gets the user's IP address
-     *
-     * @return string The IP of the user
-     */
-    public function getIp();
+        public function get();
+        public function path();
+        public function getVerb();
+        public function post($name);
+        public function getUrl();
+        public function getIp();
 }
