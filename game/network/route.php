93493225aaab7d5247459ca61186846d7ebabab6
gratefuldeadty <gratefuldeadty@gmail.com>
2014-11-09 02:08:58 -0500

    Update route.php

--- game/network/route.php
+++ game/network/route.php
@@ -1,54 +1,35 @@
- * This class represents a single endpoint
-*
-* @category OpCacheGUI
-* @package Network
-* @author Pieter Hordijk <info@pieterhordijk.com>
-*/
+<?php
+
+namespace game\network;
+
 class Route
 {
-/**
-* @var string The identifier of this route
-*/
-private $identifier;
-/**
-* @var string The verb of this route
-*/
-private $verb;
-/**
-* @var callable The callback to execute when this route matches
-*/
-private $callback;
-/**
-* Creates instance
-*
-* @param string $identifier The identifier of this route
-* @param string $verb The verb of this route
-* @param callable $callback The callback to execute when this route matches
-*/
-public function __construct($identifier, $verb, callable $callback)
-{
-$this->identifier = $identifier;
-$this->verb = $verb;
-$this->callback = $callback;
-}
-/**
-* Checks whether the current request matches the route
-*
-* @param string $identifier The identifier of this route
-* @param string $verb The verb of this route
-*/
-public function matchesRequest($identifier, $verb)
-{
-return $this->identifier === $identifier && $this->verb === $verb;
-}
-/**
-* Runs the route
-*
-* @return mixed The result of the callback
-*/
-public function run()
-{
-$callback = $this->callback;
-return $callback();
-}
+        private $identifier;
+        private $verb;
+        private $callback;
+        
+        public function __construct($identifier, $verb, callable $callback)
+        {
+                $this->identifier = $identifier;
+                $this->verb = $verb;
+                $this->callback = $callback;
+        }
+        
+        /**
+         * Check whether the current request matches the route.
+         */
+        public function matchesRequest($identifier, $verb)
+        {
+                return $this->identifier === $identifier && $this->verb === $verb;
+        }
+         
+        /**
+         * Run the route.
+         */
+        public function run()
+        {
+                $callback = $this->callback;
+                return $callback();
+        }
+        
 }
