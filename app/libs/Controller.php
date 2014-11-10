53a40fd74014e43d2f0fa4cb144499a114bd130d
gratefuldeadty <gratefuldeadty@gmail.com>
2014-11-10 04:15:42 -0500

    Update Controller.php

--- app/libs/Controller.php
+++ app/libs/Controller.php
@@ -1,1 +1,35 @@
 <?php
+
+class Controller
+{
+        function __construct()
+        {
+                Session::init();
+                try
+                {
+                        $this->dbh = new Database();
+                }
+                catch (PDOException $e)
+                {
+                        die('Database connection could not be established!')
+                }
+                $this->view = new View(); // load the view object.
+        }
+        
+        /**
+         * Load the model
+         * @param string $name
+         * @return object
+         */
+        public function loadModel($name)
+        {
+                $path = 'application/models/' . $name . '.model.php';
+                if (is_readable($path))
+                {
+                        require $path;
+                        $model = $name . 'Model';
+                        return new $model($this->dbh); // load the new model object
+                }
+        }
+}
+
