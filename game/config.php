a8c4641a274fa829905ccf0f9396f1b0544b22f2
gratefuldeadty <gratefuldeadty@gmail.com>
2014-11-12 00:47:13 -0500

    Update config.php

--- game/config.php
+++ game/config.php
@@ -1,1 +1,22 @@
 <?php
+
+// Default Elitewars RPG Configuration File 
+// @return array
+
+return new GameConfig([
+    'database' => [
+        'adapter' => 'Mysql',
+        'host' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'dbname' => 'dbname',
+    ],
+    'application' => [
+        'controllerDir' => __DIR__ . '/../controllers/',
+        'modelsDir' => __DIR__ . '/..models/',
+        'viewsDir' => __DIR__ . '/..views/',
+        'libDir' => __DIR__ . '/..library/',
+        'pluginsDir' => __DIR__ . '/../plugin/',
+        'baseUri' => __DIR__ . '/'
+    ]
+]);
